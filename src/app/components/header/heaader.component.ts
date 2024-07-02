13ccdbacaac6f7fa8a87db81fc731308be7121d3
JC <juancarlostorresmiranda96@gmail.com>
2024-07-02 17:52:46 -0500

    :version inicial 0.9.2

--- src/app/components/header/heaader.component.ts
+++ src/app/components/header/heaader.component.ts
@@ -1,21 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CargarScriptsService } from '../../services/CargarScripts/cargar-scripts.service';
+import $ from 'jquery';
+
+
 @Component({
   selector: 'app-heaader',
   templateUrl: './heaader.component.html',
   styleUrl: './heaader.component.css'
 })
-export class HeaaderComponent  {
-  /*
-constructor(private _scripts:CargarScriptsService){
+export class HeaaderComponent implements OnInit,AfterViewInit{
+  dynamicRoute: string = '/';
 
-    this.cargarScripts();
-
-}
   
-cargarScripts(){
-  //metodo para agregar scripts js
-  this._scripts.cargar(['form_validation']);
-}*/
 
+  constructor(private _scripts:CargarScriptsService){}
+
+  ngOnInit(): void {
+   this._scripts.cargar(['main']);
+
+    let carrucel = [
+      {
+          img: '/assets/backgrounds/fondo-main.png',
+          titulo: "Medio Ambiente, Cambio Climático y Sostenibilidad",
+          descrip: "Diseñamos e Implementamos el Sistema de gestión ambiental conforme a la ISO 14001:2015, ayudando a las organizaciones a gestionar sus impactos ambientales, cumplir con la normativa aplicable y avanzar hacia prácticas más sostenibles.",
+          ruta: '/medio-ambiente'
+      },
+      {
+          img: '/assets/backgrounds/slier2.png',
+          titulo: "Seguridad y Salud",
+          descrip: "Diseñamos e implementamos el Sistema de gestión de SST conforme a la naturaleza de su organización y a la Ley N° 29783, Ley de SST, ayudando a las empresas a cumplir con la legislación vigente, mejorar las condiciones laborales y reducir los riesgos de accidentes y enfermedades ocupacionales; que contribuye a crear un ambiente de trabajo seguro y saludable, lo que se traduce en una fuerza laboral más comprometida y productiva.",
+          ruta: '/seguridad-salud'
+      },
+      {
+          img: '/assets/backgrounds/slider3.png',
+          titulo: "Gestión de Riesgos Ocupacionales",
+          descrip: "Efectuamos y garantizamos un estricto seguimiento a la empresa constructora, para que el proyecto se ejecute cumpliendo el Expediente Técnico, con la calidad y seguridad, en el tiempo y con el costo estipulados en el Contrato.",
+          ruta: '/supervision'
+      }
+
+  ];
+    // Variables para manejar el estado del carrusel
+    var cont: number = 0;
+    let totalImages: number = carrucel.length;
+    let intervalId: any; // Tipo 'any' porque el tipo exacto depende de cómo se usará intervalId
+    // Elementos del DOM para modificar
+    let fondo = $('.fondo');
+    let title = $('.title-c');
+    let descrip = $('.descrip-c');
+    const showImage = (index: number): void => {
+      // Mostrar nueva imagen después de ocultar título y descripción
+      setTimeout(() => {
+          fondo.addClass('fade-out');
+          setTimeout(() => {
+              fondo.attr('src', carrucel[index].img).removeClass('fade-out');
+              title.text(carrucel[index].titulo);
+              descrip.text(carrucel[index].descrip).css('transform', 'translateY(0)');
+              this.dynamicRoute=carrucel[index].ruta
+          }, 300); 
+      }, 300); 
+  
+      /*
+      clearInterval(intervalId);
+      startInterval();
+      */
+  }
+  
+    // Mostrar la primera imagen al cargar la página
+    showImage(cont);
+    // Obtener elementos jQuery
+    const arr1 = $('.arr1');
+    const arr2 = $('.arr2');
+
+    // Función para manejar el clic en .arr1
+    arr1.click(() => {
+      cont = (cont - 1 + totalImages) % totalImages;
+            showImage(cont);
+    });
+
+    // Función para manejar el clic en .arr2
+    arr2.click(() => {
+      cont = (cont + 1) % totalImages;
+            showImage(cont);
+    });
+
+
+
+  }
+
+
+
+  ngAfterViewInit(): void {
+  
+  }
+
+
+
+ 
 }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,134 @@
 $(document).ready(function() {
-    consol.log('hola mundo');
+    const arrow = $('.arrow');
+    const arr1 = $('.arr1');
+    const arr2 = $('.arr2');
+
+
+    const slider = $('.slider');
+    if (window.innerWidth > 991) {
+        arrow.hide();
+        slider.mouseenter(() => {
+            arrow.fadeIn();
+            arrow.css('margin', '0px');
+          
+        }).mouseleave(() => { 
+            arrow.fadeOut();
+            arr1.css('margin-left', '15px');
+            arr2.css('margin-right', '15px');
+        });
+        console.log("innerWidth > 900");
+    }else{
+        arrow.show();
+        console.log("show");
+
+    }
+
+
+    // Validar tamaño de pantalla al cargar y redimensionar
+ /*   window.addEventListener('resize', () => {
+      if (window.innerWidth <= 900) {
+     //   arrow.hide();
+      }
+    });
+*/
+    /*--------------------------------- 
+     // Arreglo de objetos JSON para el carrusel
+     let carrucel = [
+        {
+            'img': '/assets/backgrounds/fondo-main.png',
+            'titulo': "Medio Ambiente, Cambio Climático y Sostenibilidad",
+            'descrip': "Diseñamos e Implementamos el Sistema de gestión ambiental conforme a la ISO 14001:2015, ayudando a las organizaciones a gestionar sus impactos ambientales, cumplir con la normativa aplicable y avanzar hacia prácticas más sostenibles.",
+            'ruta': '[/medio-ambiente]'
+        },
+        {
+            'img': '/assets/backgrounds/slier2.png',
+            'titulo': "Seguridad y Salud",
+            'descrip': "Diseñamos e implementamos el Sistema de gestión de SST conforme a la naturaleza de su organización y a la Ley N° 29783, Ley de SST, ayudando a las empresas a cumplir con la legislación vigente, mejorar las condiciones laborales y reducir los riesgos de accidentes y enfermedades ocupacionales; que contribuye a crear un ambiente de trabajo seguro y saludable, lo que se traduce en una fuerza laboral más comprometida y productiva.",
+              'ruta': '[/seguridad-salud]'
+        },
+        {
+            'img': '/assets/backgrounds/slider3.png',
+            'titulo': "Gestión de Riesgos Ocupacionales",
+            'descrip': "Efectuamos y garantizamos un estricto seguimiento a la empresa constructora, para que el proyecto se ejecute cumpliendo el Expediente Técnico, con la calidad y seguridad, en el tiempo y con el costo estipulados en el Contrato.",
+              'ruta': '[/supervision]'
+        }
+    ];
+
+    
+    // Variables para manejar el estado del carrusel
+    let cont = 0;
+    let totalImages = carrucel.length;
+    let intervalId;
+
+    // Elementos del DOM para modificar
+    let fondo = $('.fondo');
+    let title = $('.title-c');
+    let descrip = $('.descrip-c');
+    let ruta = $('.btn_info');
+
+    function showImage(index) {
+  
+
+        // Mostrar nueva imagen después de ocultar título y descripción
+        setTimeout(function() {
+            fondo.addClass('fade-out');
+            setTimeout(function() {
+                fondo.attr('src', carrucel[index].img).removeClass('fade-out');
+                title.text(carrucel[index].titulo)
+                descrip.text(carrucel[index].descrip).css('transform', 'translateY(0)');
+                ruta.attr('[routerLink]',carrucel[index].ruta);
+                console.log(carrucel[index].ruta.toString);
+            }, 300); 
+        }, 300); 
+
+        /*
+        clearInterval(intervalId);
+        startInterval();
+        
+    }
+
+    // Mostrar la primera imagen al cargar la página
+    showImage(cont);
+
+    // Manejar clics en las flechas de navegación
+    $('.arr1').click(function() {
+        cont = (cont - 1 + totalImages) % totalImages;
+        showImage(cont);
+    });
+
+    $('.arr2').click(function() {
+        cont = (cont + 1) % totalImages;
+        showImage(cont);
+    });
+
+    /*
+    // Función para iniciar el intervalo de cambio automático
+    function startInterval() {
+        intervalId = setInterval(function() {
+            $('.arr2').click(); // Simula un clic en la flecha derecha para avanzar automáticamente
+        }, 10000); // Intervalo de cambio automático en milisegundos (10 segundos en este caso)
+    }
+
+    // Iniciar el intervalo automáticamente al cargar la página
+    startInterval();
+
+*/
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
 })
